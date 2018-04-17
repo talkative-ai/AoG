a730bd7f7a53206686e4fa56ecf8279785929c2a
Wyatt Arent <hello@wyatt.engineer>
2018-04-17 10:31:43 -0400

    Expand types

--- request.go
+++ request.go
@@ -1,41 +1,62 @@
 package aog
 
 type Request struct {
-	Inputs []struct {
-		Arguments []struct {
-			RawText   string `json:"rawText"`
-			TextValue string `json:"textValue"`
-			Name      string `json:"name"`
-		} `json:"arguments"`
-		Intent    string `json:"intent"`
-		RawInputs []struct {
-			Query     string      `json:"query"`
-			InputType interface{} `json:"inputType"`
-		} `json:"rawInputs"`
-	} `json:"inputs"`
-	User struct {
-		UserID  string `json:"userId"`
-		Profile struct {
-			DisplayName string `json:"display_name"`
-			GivenName   string `json:"given_name"`
-			FamilyName  string `json:"family_name"`
-		} `json:"profile"`
-		AccessToken string `json:"accessToken"`
-	} `json:"user"`
-	Device struct {
-		Location struct {
-			Coordinates struct {
-				Latitude  float64 `json:"latitude"`
-				Longitude float64 `json:"longitude"`
-			} `json:"coordinates"`
-			FormattedAddress string `json:"formattedAddress"`
-			ZipCode          string `json:"zipCode"`
-			City             string `json:"city"`
-		} `json:"location"`
-	} `json:"device"`
-	Conversation struct {
-		ConversationToken string      `json:"conversationToken"`
-		ConversationID    string      `json:"conversationId"`
-		Type              interface{} `json:"type"`
-	} `json:"conversation"`
+	Inputs       []Input      `json:"inputs"`
+	User         User         `json:"user"`
+	Device       Device       `json:"device"`
+	Conversation Conversation `json:"conversation"`
 }
+
+type Input struct {
+	Arguments []InputArgument `json:"arguments"`
+	Intent    string          `json:"intent"`
+	RawInputs []RawInput      `json:"rawInputs"`
+}
+
+type InputArgument struct {
+	RawText   string `json:"rawText"`
+	TextValue string `json:"textValue"`
+	Name      string `json:"name"`
+}
+
+type RawInput struct {
+	Query     string      `json:"query"`
+	InputType interface{} `json:"inputType"`
+}
+
+type User struct {
+	UserID      string      `json:"userId"`
+	Profile     UserProfile `json:"profile"`
+	AccessToken string      `json:"accessToken"`
+}
+
+type UserProfile struct {
+	DisplayName string `json:"display_name"`
+	GivenName   string `json:"given_name"`
+	FamilyName  string `json:"family_name"`
+}
+
+type Conversation struct {
+	ConversationToken string      `json:"conversationToken"`
+	ConversationID    string      `json:"conversationId"`
+	Type              interface{} `json:"type"`
+}
+
+type Device struct {
+	Location DeviceLocation `json:"location"`
+}
+type DeviceLocation struct {
+	Coordinates      DeviceLocationCoordinates `json:"coordinates"`
+	FormattedAddress string                    `json:"formattedAddress"`
+	ZipCode          string                    `json:"zipCode"`
+	City             string                    `json:"city"`
+}
+
+type DeviceLocationCoordinates struct {
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
+
+const ConstIntentText = "actions.intent.TEXT"
+const ConstInputTypeKeyboard = "KEYBOARD"
+const ConstInputArgumentText = "text"
